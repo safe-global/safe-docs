f66f61fc9dcc0f2e3a87dd9cd943d6f65867f8a3
Denis Granha <denisgranha@gmail.com>
2020-02-28 17:30:41 +0100

    add baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Gnosis Developer Portal Safe', // Title for your website.
   tagline: 'Safe',
   url: 'https://safe-docs.gnosis.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/safe/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
