6efc1b569bad7e3a852e517d26f58af63f7a0f6b
louis-md <louis@margot-duclot.fr>
2023-11-28 09:56:02 +0100

    Add index page

---
+++ components/Home.tsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.css'
+
+const Home = () => (
+  <div>
+    <h1>Safe Docs Landing page</h1>
+    <a href='/getting-started/what-is-safe' className={styles.link}>Get started</a>
+  </div>
+)
+
+export default Home
