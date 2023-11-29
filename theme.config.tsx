3b7272b14048bec0d3a09b04f8837a233813d024
louis-md <louis@margot-duclot.fr>
2023-11-29 18:31:01 +0100

    Implement design feedback (navbar background color & backdrop filter)

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import Img from 'next/image'
 
-import SafeLogo from './assets/svg/safe-logo.svg'
-import Twiter from './assets/svg/x-icon.svg'
+import SafeLogo from './assets/svg/safe-logo-white.svg'
 import Footer from './components/Footer'
 
 const config: DocsThemeConfig = {
