563bb5cdfd57d5d81aef73a9cea549270d0929e6
louis-md <louis@margot-duclot.fr>
2023-12-04 14:27:11 +0100

    Fix deployment workflow

--- .github/scripts/s3_upload.sh
+++ .github/scripts/s3_upload.sh
@@ -4,7 +4,10 @@ set -ev
 
 aws s3 sync ./out $BUCKET --delete
 
-# Finally, upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
+# Upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
+
+cd out
+
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
     aws s3 cp ${file%} $BUCKET/${file%.*} --content-type 'text/html'
 done
