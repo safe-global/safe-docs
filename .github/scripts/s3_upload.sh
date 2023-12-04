785c14d284422dae2ab95831cd42e12052f1ab8f
louis-md <louis@margot-duclot.fr>
2023-12-04 12:09:34 +0100

    Fix deployment workflow

--- .github/scripts/s3_upload.sh
+++ .github/scripts/s3_upload.sh
@@ -3,13 +3,13 @@
 set -ev
 
 # First, upload the new files w/o deleting the old ones
-aws s3 sync . $BUCKET --exclude '.git/*' --exclude '.github/*'
+aws s3 sync ./.next $BUCKET
 
 # Second, upload them again but delete the old files this time
 # This allows for a no-downtime deployment
-aws s3 sync . $BUCKET --exclude '.git/*' --exclude '.github/*' --delete
+aws s3 sync ./.next $BUCKET --delete
 
 # Finally, upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
-    aws s3 cp ${file%} $BUCKET/${file%.*} --content-type 'text/html' --exclude '.git/*' --exclude '.github/*'
+    aws s3 cp ${file%} $BUCKET/${file%.*} --content-type 'text/html'
 done
