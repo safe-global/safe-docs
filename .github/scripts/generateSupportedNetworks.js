ee983418f5b233b47f081a573b43b8a861457eee
louis-md <louis@margot-duclot.fr>
2023-12-05 14:38:33 +0100

    Add toruscan as deprecated block explorer

--- .github/scripts/generateSupportedNetworks.js
+++ .github/scripts/generateSupportedNetworks.js
@@ -141,5 +141,6 @@ const deprecatedBlockExplorers = [
   'https://evm.explorer.canto.io',
   'https://explorer.autobahn.network',
   'https://explorer.cascadia.foundation',
-  'https://testnet.torusscan.com'
+  'https://testnet.torusscan.com',
+  'https://testnet.toruscan.com'
 ]
