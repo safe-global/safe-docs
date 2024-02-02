8d72dd911bd4a7b14f4801fe60ea4d4237e64133
Tanay Pant <7481165+tanay1337@users.noreply.github.com>
2024-02-02 12:16:19 +0100

    Minor fixes (#334)
    * Minor fixes
    
    * Change title of index page
    
    * Add a community resource
    
    * Rename Nextra Feedback to Docs Feedback in GH template
    
    * Remove youtube cookies from embedded player; fix responsive design in resource hub
    
    ---------
    
    Co-authored-by: Tanay Pant <tanaypantprotonmail.com>
    Co-authored-by: louis-md <louis@margot-duclot.fr>

--- components/ResourceHub/Resources.tsx
+++ components/ResourceHub/Resources.tsx
@@ -255,7 +255,7 @@ export const Resources = (): ReactElement => {
           <TextField
             className={css.searchField}
             variant='outlined'
-            placeholder='Search by name, description or tag'
+            placeholder='Search by name, description, or tag'
             InputProps={{
               startAdornment: (
                 <InputAdornment position='start'>
@@ -278,7 +278,7 @@ export const Resources = (): ReactElement => {
             }}
             fullWidth
           />
-          <Typography mt={2}>
+          <Typography textAlign={['center', 'left']} mt={2}>
             <Typography component='span' color='primary.light'>
               Example:
             </Typography>{' '}
@@ -437,7 +437,7 @@ export const Resources = (): ReactElement => {
                   mx='auto'
                   textAlign='center'
                 >
-                  Listings are not endorsements and are only for informational
+                  Listings are not endorsements and are only for educational
                   purposes.
                 </Typography>
               </Grid>
