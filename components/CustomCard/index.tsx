c7c4d50daef93b2855618b5f0ee53c87e68f1794
Tanay Pant <7481165+tanay1337@users.noreply.github.com>
2024-02-12 18:10:53 +0100

    Add the new support page (#355)
    * Add the new support page
    
    * Add link to Resource Hub
    
    * Improve the UI
    
    ---------
    
    Co-authored-by: Tanay Pant <tanaypantprotonmail.com>

---
+++ components/CustomCard/index.tsx
@@ -0,0 +1,51 @@
+import { Card, Typography, Grid } from '@mui/material'
+import ArrowIcon from '../../assets/svg/arrow-outward.svg'
+
+import css from './styles.module.css'
+
+const CustomCard: React.FC<{
+  title: string
+  description: string
+  url: string
+  icon: JSX.Element
+}> = ({ title, description, url, icon }) => (
+  <Card
+    sx={{
+      transition: 'all 0.2s ease-in-out',
+      border: '1px solid',
+      borderColor: 'transparent',
+      '&:hover': {
+        borderColor: 'secondary.light'
+      },
+      width: '100%'
+    }}
+    className={css.card}
+  >
+    <a href={url} target='_blank' rel='noreferrer' style={{ width: '100%' }}>
+      <div style={{ width: '100%' }}>
+        <Grid
+          item
+          container
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+        >
+          {icon}
+          <ArrowIcon className={css.icon} />
+          <Typography fontWeight='500' variant='h4' mt={1} mb={1}>
+            {title}
+          </Typography>
+        </Grid>
+        <Typography
+          variant='body2'
+          color='text.secondary'
+          className={css.description}
+        >
+          {description}
+        </Typography>
+      </div>
+    </a>
+  </Card>
+)
+
+export default CustomCard
