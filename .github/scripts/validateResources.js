edee0a7863b34bfc596edfebbcdb47bfeabb2896
louis-md <louis@margot-duclot.fr>
2024-03-21 14:56:15 +0100

    Test workflow validation for resource hub

--- .github/scripts/validateResources.js
+++ .github/scripts/validateResources.js
@@ -5,28 +5,29 @@ const communityResourcesPath =
   './components/ResourceHub/community-resources.json'
 
 const validateResource = (resource, index, resources) => {
+  const errorMessage = 'community-resources.json:1:1:'
   if (resources.findIndex(r => r.url === resource.url) !== index) {
-    console.log(`Duplicate resource: ${resource.url}`)
+    console.log(errorMessage + `Duplicate resource: ${resource.url}`)
     return false
   }
   if (!resource.name) {
-    console.log(`Resource name is missing: ${resource.url}`)
+    console.log(errorMessage + `Resource name is missing: ${resource.url}`)
     return false
   }
   if (!resource.type) {
-    console.log(`Resource type is missing: ${resource.url}`)
+    console.log(errorMessage + `Resource type is missing: ${resource.url}`)
     return false
   }
   if (!resource.date) {
-    console.log(`Resource date is missing: ${resource.url}`)
+    console.log(errorMessage + `Resource date is missing: ${resource.url}`)
     return false
   }
   if (!resource.description) {
-    console.log(`Resource description is missing: ${resource.url}`)
+    console.log(errorMessage + `Resource description is missing: ${resource.url}`)
     return false
   }
   if (!resource.tags || !resource.tags.length) {
-    console.log(`Resource tags are missing: ${resource.url}`)
+    console.log(errorMessage + `Resource tags are missing: ${resource.url}`)
     return false
   }
   return true
