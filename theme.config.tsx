a5d28f05a964c157655e11f13b63aedd51ff60e4
Tanay Pant <7481165+tanay1337@users.noreply.github.com>
2024-03-26 13:17:45 +0100

    feat: add custom og for RH, copy edit 404, and fix a bug (#417)
    * feat: add custom og for RH, copy edit 404, and fix a bug
    
    * fix: add the Resource Hub image
    
    ---------
    
    Co-authored-by: Tanay Pant <tanaypantprotonmail.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -32,6 +32,12 @@ const config: DocsThemeConfig = {
   head: <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />,
   useNextSeoProps: () => {
     const { asPath } = useRouter()
+    let ogImagePath = 'og_image.png'
+    switch (asPath) {
+      case '/resource-hub':
+        ogImagePath = 'og_image_resource_hub.png'
+        break
+    }
     return {
       noindex: process.env.NEXT_PUBLIC_IS_PRODUCTION !== 'true',
       nofollow: process.env.NEXT_PUBLIC_IS_PRODUCTION !== 'true',
@@ -45,7 +51,7 @@ const config: DocsThemeConfig = {
           'Safe{Core} is an open-source and modular account abstraction stack. Learn about its features and how to use it.',
         images: [
           {
-            url: `${process.env.NEXT_PUBLIC_HOST_URL}/og_image.png`,
+            url: `${process.env.NEXT_PUBLIC_HOST_URL}/${ogImagePath}`,
             width: 1200,
             height: 672,
             alt: 'Safe Logo',
