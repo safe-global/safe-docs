4ecaf27215a1894dcd55749a0a0a77a60230f21a
Tanay Pant <7481165+tanay1337@users.noreply.github.com>
2024-03-26 16:18:15 +0100

    Revert "Redirect index to what-is-safe server-side (#420)" (#421)
    This reverts commit 6b7fc77f609b390d4d77fd2c81fcb28cc9947843.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import type { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-const RedirectIndex: React.FC = () => <></>
+const RedirectIndex: React.FC = () => {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    void push('/home/what-is-safe')
+  }, [push])
+
+  return <></>
+}
 
 export default RedirectIndex
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    redirect: {
-      destination: '/home/what-is-safe',
-      permanent: true
-    }
-  }
-}
