774412589673b14f02ea4632c4ab35e6c0119e2d
louis-md <louis@margot-duclot.fr>
2024-05-07 08:57:13 +0200

    Add google tag manager (#464)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import type { AppProps } from 'next/app'
 import { useEffect, type ReactElement } from 'react'
 import Head from 'next/head'
 import ReactGA from 'react-ga4'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 import MetaTags from '../components/MetaTags'
 import { CookieBanner } from '../components/CookieBanner'
@@ -73,6 +74,9 @@ const App = ({
       <Head>
         <MetaTags path={router.asPath} />
       </Head>
+      <GoogleTagManager
+        gtmId={String(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID)}
+      />
       <CacheProvider value={emotionCache}>
         <CssVarsProvider theme={cssVarsTheme}>
           <CookieBannerContextProvider>
