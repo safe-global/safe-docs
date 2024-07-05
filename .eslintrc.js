510c32b1e64d82006368e7c2bc84b73da5ffa172
louis-md <louis@margot-duclot.fr>
2024-07-05 12:35:06 +0200

    Add 7579 tutorial (#514)
    * Add 7579 tutorial
    
    * Move example code in its own top-level folder
    
    * Ignore type validation and linter for example files
    
    * Fix vale errors
    
    * Fix typo
    
    * Implement requested changes
    
    * Edit screenshots
    
    * Update pages/advanced/erc-7579/tutorials/7579-tutorial.mdx
    
    Co-authored-by: Konrad <kopp.konrad@gmail.com>
    
    * Implement requested changes
    
    * Implement requested changes
    
    * Implement requested changes
    
    * Fix deploy workflows
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Konrad <kopp.konrad@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  ignorePatterns: ['.eslintrc.js', 'next.config.js', 'next-env.d.ts', 'out'],
+  ignorePatterns: ['.eslintrc.js', 'next.config.js', 'next-env.d.ts', 'out', '**/examples/**'],
   rules: {
     '@typescript-eslint/key-spacing': 0,
     'multiline-ternary': 0,
