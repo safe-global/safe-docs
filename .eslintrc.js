ef1de12084e53289d2747970fdd6abedd7e84eb9
valle-xyz <valentin.seehausen@gmail.com>
2024-07-26 16:55:25 +0200

    update tsconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:security/recommended-legacy",
-    "standard-with-typescript",
-    "next/core-web-vitals",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended-legacy',
+    'standard-with-typescript',
+    'next/core-web-vitals'
   ],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json'
   },
   ignorePatterns: [
-    ".eslintrc.js",
-    "next.config.js",
-    "next-env.d.ts",
-    "out",
-    "components/ApiReference/examples/*.ts",
+    '.eslintrc.js',
+    'next.config.js',
+    'next-env.d.ts',
+    'out',
+    '**/examples/**'
   ],
   rules: {
-    "@typescript-eslint/key-spacing": 0,
-    "multiline-ternary": 0,
-    "no-console": ["error", { allow: ["info", "warn", "error"] }],
-    "max-len": [
+    '@typescript-eslint/key-spacing': 0,
+    'multiline-ternary': 0,
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'max-len': [
       2,
       {
         code: 100,
         ignoreComments: true,
         ignoreStrings: true,
         ignoreTemplateLiterals: true,
-        ignoreUrls: true,
-      },
-    ],
-  },
-};
+        ignoreUrls: true
+      }
+    ]
+  }
+}
