6ecf4b3aec7c8e8210917bff98b065688423a08c
valle-xyz <valentin.seehausen@gmail.com>
2024-07-29 21:56:37 +0200

    add TS to safe by owners

--- components/ApiReference/examples/-v1-owners-{address}-safes--get.ts
+++ components/ApiReference/examples/-v1-owners-{address}-safes--get.ts
@@ -1,1 +1,11 @@
-const safes: OwnerResponse = await apiKit.getSafesByOwner(ownerAddress)
+import SafeApiKit from '@safe-global/api-kit'
+
+const apiKit = new SafeApiKit({
+  chainId: 11155111n
+})
+
+const safes = await apiKit.getSafesByOwner(
+  '0xa6d3DEBAAB2B8093e69109f23A75501F864F74e2'
+)
+
+console.log(safes)
