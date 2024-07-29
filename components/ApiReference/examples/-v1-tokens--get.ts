baaac874e9e2fd46ec84e5ba47b522e89f86c535
valle-xyz <valentin.seehausen@gmail.com>
2024-07-29 22:24:20 +0200

    add TS to token section

--- components/ApiReference/examples/-v1-tokens--get.ts
+++ components/ApiReference/examples/-v1-tokens--get.ts
@@ -1,1 +1,9 @@
-const tokens: TokenInfoListResponse = await apiKit.getTokenList()
+import SafeApiKit from '@safe-global/api-kit'
+
+const apiKit = new SafeApiKit({
+  chainId: 11155111n
+})
+
+const tokenList = await apiKit.getTokenList()
+
+console.log(tokenList)
--- components/ApiReference/examples/-v1-tokens-{address}--get.ts
+++ components/ApiReference/examples/-v1-tokens-{address}--get.ts
@@ -1,1 +1,11 @@
-const token: TokenInfoResponse = await apiKit.getToken(tokenAddress)
+import SafeApiKit from '@safe-global/api-kit'
+
+const apiKit = new SafeApiKit({
+  chainId: 11155111n
+})
+
+const token = await apiKit.getToken(
+  '0x687e43D0aB3248bDfebFE3E8f9F1AB2B9FcE982d'
+)
+
+console.log(token)
