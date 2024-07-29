05f25e7aa434002ddb2a2136f9df5fe64ab0ecfb
valle-xyz <valentin.seehausen@gmail.com>
2024-07-29 22:21:15 +0200

    add TS to safe section

--- components/ApiReference/examples/-v1-safes-{address}--get.ts
+++ components/ApiReference/examples/-v1-safes-{address}--get.ts
@@ -1,1 +1,11 @@
-const safeInfo: SafeInfoResponse = await apiKit.getSafeInfo(safeAddress)
+import SafeApiKit from '@safe-global/api-kit'
+
+const apiKit = new SafeApiKit({
+  chainId: 11155111n
+})
+
+const safeInfo = await apiKit.getSafeInfo(
+  '0x5298A93734C3D979eF1f23F78eBB871879A21F22'
+)
+
+console.log(safeInfo)
--- components/ApiReference/examples/-v1-safes-{address}-creation--get.ts
+++ components/ApiReference/examples/-v1-safes-{address}-creation--get.ts
@@ -1,3 +1,11 @@
-const safeCreationInfo: SafeCreationInfoResponse = await apiKit.getSafeCreationInfo(
-  safeAddress
+import SafeApiKit from '@safe-global/api-kit'
+
+const apiKit = new SafeApiKit({
+  chainId: 11155111n
+})
+
+const safeCreationInfo = await apiKit.getSafeCreationInfo(
+  '0x5298A93734C3D979eF1f23F78eBB871879A21F22'
 )
+
+console.log(safeCreationInfo)
