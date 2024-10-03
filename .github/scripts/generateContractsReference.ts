d52dc909f3039b5f5dd2bd46035fb250400aef6e
louis-md <louis@margot-duclot.fr>
2024-10-03 18:34:58 +0200

    Generate markdown with hardhat

--- .github/scripts/generateContractsReference.ts
+++ .github/scripts/generateContractsReference.ts
@@ -19,48 +19,55 @@ const walkPath = (dir: string) => {
   return results
 }
 
-const cloneRepo = (repoUrl: string, repoDestination: string) => {
+const cloneRepo = (
+  repoUrl: string,
+  repoDestination: string,
+  mdDestination: string
+) => {
+  const isModules = repoDestination === 'modules'
   const source = repoDestination + '/' + repoDestination
   shell.exec(`git clone ${repoUrl} ${repoDestination}`)
   const paths = walkPath(source)
     .map(p => p.replace(source, ''))
     .filter(p => !p.includes('test/'))
 
-  const natspec = getContractsNatspec(paths, repoDestination, source)
+  if (!isModules) {
+    shell.exec(`cd ${repoDestination} && git checkout release/v1.4.1`)
+  }
 
-  shell.rm('-rf', repoDestination)
-  return natspec
-}
-
-const getContractsNatspec = (
-  paths: string[],
-  type: string,
-  destination: string
-) => {
-  const contracts: any = []
-
-  paths
-    .filter(p => p.endsWith('.sol'))
-    .forEach(p => {
-      const natspec = shell
-        .exec(`solc --userdoc ${destination}/${p}`)
-        .stdout.split('======= contracts/contracts/')[1]
-        .split('=======')[1]
-        .split('User Documentation')[1]
-        .trim()
-      contracts.push({ ...JSON.parse(natspec), contract: p })
-    })
-
-  fs.writeFileSync(
-    'components/ContractsReference/' + type + '.json',
-    JSON.stringify(contracts, null, 2),
-    'utf8'
+  shell.exec(
+    `cd ${repoDestination} && ${
+      isModules ? 'p' : 'npm i && '
+    }npm i @solarity/hardhat-markup`
   )
+
+  const hardhatConfigPath = `${repoDestination}/hardhat.config.ts`
+  const hardhatConfig = fs.readFileSync(hardhatConfigPath, 'utf8')
+  const newHardhatConfig =
+    'import "@solarity/hardhat-markup";\n' + hardhatConfig
+  fs.writeFileSync(hardhatConfigPath, newHardhatConfig, 'utf8')
+
+  shell.exec(`cd ${repoDestination} && npx hardhat markup`)
+  shell.rm('-rf', mdDestination)
+  shell.cp(
+    '-R',
+    `${repoDestination}/generated-markups/contracts`,
+    mdDestination
+  )
+  shell.rm('-rf', 'contracts')
 }
 
 const main = () => {
-  cloneRepo(`https://github.com/safe-global/safe-smart-account/`, 'contracts')
-  cloneRepo(`https://github.com/safe-global/safe-modules/`, 'modules')
+  cloneRepo(
+    `https://github.com/safe-global/safe-smart-account/`,
+    'contracts',
+    'pages/smart-contracts/smart-account-reference'
+  )
+  cloneRepo(
+    `https://github.com/safe-global/safe-modules/`,
+    'modules',
+    'pages/smart-contracts/modules-reference'
+  )
 }
 
 main()
