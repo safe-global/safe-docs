cd1a8a66190d7abf506d3faf4b9eb8bb63ceb286
louis-md <louis@margot-duclot.fr>
2024-10-22 16:29:50 +0200

    Allow users to search supported networks by contract addresses

--- components/SupportedNetworks/Networks.tsx
+++ components/SupportedNetworks/Networks.tsx
@@ -380,7 +380,7 @@ const SupportedNetworks: React.FC = () => {
             <TextField
               className={css.searchField}
               variant='outlined'
-              placeholder='Search by network name or chain ID'
+              placeholder='Search by network name, chain ID or contract address'
               InputProps={{
                 style: {
                   color: 'white',
