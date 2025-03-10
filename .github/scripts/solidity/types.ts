005b6340710806e00ea7aa9c1c029a8435b89159
louis-md <louis@margot-duclot.fr>
2025-03-10 20:47:01 +0100

    Move script solidity functions to `lib` folder

--- .github/scripts/solidity/types.ts
+++
@@ -1,7 +0,0 @@
-export type ParamType = { name: string; type: string; description: string }
-export type DocContent = {
-  contractName: string
-  eventName?: string
-  functionName?: string
-  contents: string
-}
