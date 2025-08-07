c70fef9eba1fe3264d2c9ba2b606fe3923ff90d6
Daniel <25051234+dasanra@users.noreply.github.com>
2025-08-07 18:49:20 +0200

    fix: remove Try swagger button in Safe Decoder Service

--- components/ApiReference/SampleRequestHeader.tsx
+++ components/ApiReference/SampleRequestHeader.tsx
@@ -8,27 +8,30 @@ import { getSwaggerUrl } from './utils'
 const SampleRequestHeader: React.FC<{
   method: string
   pathWithParams: string
-}> = ({ method, pathWithParams }) => {
+  disableSwagger: boolean
+}> = ({ method, pathWithParams, disableSwagger }) => {
   const [network] = useContext(NetworkContext)
 
   return (
     <Grid container justifyContent='space-between' alignItems='center'>
       <MdxHeading headingLevel={4}>Sample Request</MdxHeading>
-      <Button
-        variant='text'
-        size='small'
-        href={getSwaggerUrl(network)}
-        target='_blank'
-        rel='noopener noreferrer'
-        sx={{
-          mt: 1,
-          height: '36px',
-          color: 'rgba(249,250,251,.7)',
-          backgroundColor: ({ palette }) => palette.grey[900]
-        }}
-      >
-        Try it on Swagger
-      </Button>
+      {!disableSwagger && (
+        <Button
+          variant='text'
+          size='small'
+          href={getSwaggerUrl(network)}
+          target='_blank'
+          rel='noopener noreferrer'
+          sx={{
+            mt: 1,
+            height: '36px',
+            color: 'rgba(249,250,251,.7)',
+            backgroundColor: ({ palette }) => palette.grey[900]
+          }}
+        >
+          Try it on Swagger
+        </Button>
+      )}
     </Grid>
   )
 }
