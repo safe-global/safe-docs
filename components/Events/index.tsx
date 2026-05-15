5ad3bf639851b8605ea465a217128d1ee3e96605
Daniel <25051234+dasanra@users.noreply.github.com>
2026-05-15 16:06:03 +0200

    chore: remove events section

--- components/Events/index.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-import Loader from '../Loader'
-
-const Events = dynamic(async () => await import('./Events'), {
-  loading: () => <Loader />
-})
-
-export default Events
